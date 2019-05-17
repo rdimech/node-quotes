0598145060a4e3f0b06fada9d622fa9f3f64ab86
Robert Dimech <robertdimech@Roberts-MacBook-Pro-13.local>
2019-05-16 21:46:37 -0400

    first commit

---
+++ quotes.js
@@ -0,0 +1,9 @@
+const quotes = ["Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead", 
+"Whoever is happy will make others happy too. -Anne Frank", 
+"Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson", 
+"Never let the fear of striking out keep you from playing the game. -Babe Ruth", 
+"In the end, it's not the years in your life that count. It's the life in your years. -Abraham Lincoln"];
+
+module.exports = { 
+    quotes
+};
